2298fae8fc24a1bf337a11e48787c04e15c0d6f0
joe <zhoumiwh@ncn.ibm.com>
2022-11-01 17:13:18 +0800

    add test code for rendering html

--- task/dashboard.go
+++ task/dashboard.go
@@ -1,7 +1,19 @@
 package task
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
 type Dashboard struct {
 }
 
@@ -10,5 +22,15 @@ func NewDashboard() *Dashboard {
 }
 
 func (d *Dashboard) DashboardHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world"))
+	tmpl := template.Must(template.ParseFiles("../asset/task.html"))
+
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
 }
